c1b71cf77e782a51d5f77d18ddb10f6b311b2ed4
John Hancock <jhacock1975@gmail.com>
2023-04-17 20:59:47 -0400

    sync

--- find-all-files-containing.sh
+++ find-all-files-containing.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-# Check if there are at least 2 arguments (search terms and path)
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <word1> <word2> ... <wordN> <path>"
-  exit 1
+# Usage: find-files-containing [search_directory] [search_pattern] [grep_pattern]
+
+search_directory="$1"
+search_pattern="$2"
+grep_pattern="$3"
+
+# Check if arguments are provided
+if [ -z "$search_directory" ] || [ -z "$search_pattern" ] || [ -z "$grep_pattern" ]; then
+    echo "Usage: $0 [search_directory] [search_pattern] [grep_pattern]"
+    exit 1
 fi
 
-# Save the path as the last argument
-path="${!#}"
-
-# Remove the last argument (path) from the arguments array
-set -- "${@:1:$#-1}"
-
-# Initialize the search command with the first word and exclude patterns
-search_command="find '$path' -type f -iname '*.*' ! -iname '*.csv' ! -iname '*.log' ! -iname '*.json' -print0 | xargs -0 grep -il '$1'"
-
-# Add additional words to the search command and exclude patterns
-for word in "${@:2}"; do
-  search_command+=" | xargs -0 grep -il '$word'"
+# Use find to get the list of files and iterate through them
+find "$search_directory" -type f -name "$search_pattern" | while read -r file; do
+    # Use grep to search for the pattern in the file
+    grep -Hn --color=auto "$grep_pattern" "$file"
 done
-
-# Execute the search command
-eval "$search_command"
